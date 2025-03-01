9ca9c84c724c78e7b309aa024bff7350f6a2e5cd
uguygfgti <work.uguygfgti@gmail.com>
2025-03-01 08:58:25 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+// รอให้เว็บโหลดเสร็จ แล้วซ่อน loader
+window.addEventListener("load", function() {
+    const loaderWrapper = document.querySelector(".loader-wrapper");
+    loaderWrapper.style.opacity = "0";
+    setTimeout(() => {
+        loaderWrapper.style.display = "none";
+    }, 500); // ให้ค่อยๆจางหาย
+});
